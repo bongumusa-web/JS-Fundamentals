84ca759eb2a8a55a1b160459457e1db8a80afe06
Bongumusa Gumede <bonggums@gmail.com>
2025-11-11 23:05:30 +0200

    2-argumrnts.js

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+
+const args = process.argv.slice(2); // get only the user arguments
+
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
